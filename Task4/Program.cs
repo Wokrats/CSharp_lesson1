f303c4a1bb5c15124f39e8669dfac238b48f4dd2
Wokrats <wokrats2@gmail.com>
2024-02-15 20:07:17 +0500

    add four task

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 5;
+int b = 4;
+int c = 9;
+int d = 3;
+int e = 8;
+int max = a;
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+if (d>max)
+{
+    max = d;
+}
+if (e>max)
+{
+    max = e;
+}
+Console.Write("Maximum is: ");
+Console.WriteLine(max);
